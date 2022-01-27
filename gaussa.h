7a1454685c69eec7e21140f6449a30065ccb2af3
SexyPumpkin <62780669+SexyPumpkin@users.noreply.github.com>
2022-01-27 20:59:49 +0200

    Added description to Gauss elimination method

--- gaussa.h
+++ gaussa.h
@@ -2,5 +2,17 @@
 
 #include <vector>
 
-
+/*
+    A function that solves System of equations arr by the "Gaussian elimination" method and outputs a solution in the form of a linear array of values.
+    
+    Parameters
+    ----------
+    arr : 2d array in vector struct
+    	Array to solve, in which every line contains "y" value on last position of vector
+    
+    Returns
+    -------
+    output : vector
+    	Array of coefficients
+*/
 std::vector<double> gaussa(std::vector<std::vector<double>> arr);
